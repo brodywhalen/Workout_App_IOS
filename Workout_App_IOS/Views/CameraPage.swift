6564726c974b8b46b97b832e88b30eaec386f03f
Brody Whalen <brody.whalen.1@gmail.com>
2025-04-29 21:15:07 -0500

    deleted the media pipe code. implemented av foundation camera. Need to add some zoom functionality and then integrate the frame processor to start polling from media pipe.

---
+++ Workout_App_IOS/Views/CameraPage.swift
@@ -0,0 +1,18 @@
+//
+//  CameraPage.swift
+//  Workout_App_IOS
+//
+//  Created by Brody Whalen on 4/29/25.
+//
+
+import SwiftUI
+
+struct CameraPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CameraPage()
+}
