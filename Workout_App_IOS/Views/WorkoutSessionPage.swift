f5578acad9745fda4bb0f6e19bc9ad1f250a7be0
Brody Whalen <brody.whalen.1@gmail.com>
2025-05-28 23:33:45 -0500

    Started blocking out the live exercise tracker. I want to finish the implementation by tomorrow if possible. After this main flow is working Going to look back and polish ui. Then I want to add users admin with supabase and then move on to local challenges.

---
+++ Workout_App_IOS/Views/WorkoutSessionPage.swift
@@ -0,0 +1,24 @@
+//
+//  WorkoutSession.swift
+//  Workout_App_IOS
+//
+//  Created by Brody Whalen on 5/28/25.
+//
+import SwiftUI
+
+struct WorkoutSessionPage: View {
+    
+    //@Binding TemplateforSession: WorkoutTemplate
+    
+    
+    var body: some View {
+        ZStack {
+            Text("Hello World")
+        }
+    }
+}
+
+
+#Preview {
+    WorkoutSessionPage()
+}
