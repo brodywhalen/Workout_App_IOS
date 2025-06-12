14ecbdc79a321bd6d9e8a2e6778d9bba9d8a005b
Brody Whalen <brody.whalen.1@gmail.com>
2025-06-12 00:28:56 -0500

    started to add live activity. Will need to implement syncing mechanism and get the count up timer working.

---
+++ PersistentWorkoutActivity/Persistent_Workout_Widget.swift
@@ -0,0 +1,16 @@
+//
+//  PersistentWorkoutActivityBundle.swift
+//  PersistentWorkoutActivity
+//
+//  Created by Brody Whalen on 6/11/25.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct PersistentWorkoutActivityBundle: WidgetBundle {
+    var body: some Widget {
+
+    }
+}
